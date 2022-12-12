710048750079692c05aca1c186cf7db3b2343264
118140092 Muhammad Rizky Nilham Perdana <muhammad.118140092@student.itera.ac.id>
2022-12-12 23:47:49 +0700

    Add files via upload

---
+++ minggu-1/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Halaman Data Diri</title>
+  </head>
+  <body>
+    <h1>Data Diri</h1>
+
+    <h2>Gambar</h2>
+    <img src="https://www.gardaoto.com/wp-content/uploads/2022/09/10-Pemandangan-Alam-Paling-Indah-yang-Wajib-Kalian-Datangi.jpg" alt="Gambar">
+
+    <h2>Link</h2>
+    <a href="https://www.youtube.com">youtube</a>
+
+    <h2>Video</h2>
+    <video src="https://cdn.pixabay.com/vimeo/413229662/Air%20Terjun%20-%2037088.mp4?width=1280&expiry=1670861267&hash=99f63ac5382d012ed499d4d15de90d04e3734070" width="500" height="300" controls></video>
+
+    <h2>Tabel</h2>
+    <table border="2">
+      <tr>
+        <th>Nama</th>
+        <th>Umur</th>
+      </tr>
+      <tr>
+        <td>John Doe</td>
+        <td>30</td>
+      </tr>
+    </table>
+
+    <h2>Radiobutton</h2>
+    <label for="nama">nama :</label>
+    <input type="nama" name="nama" id="nama" /><br>
+    <input type="radio" name="jenis_kelamin" value="pria"> Pria<br>
+    <input type="radio" name="jenis_kelamin" value="wanita"> Wanita<br>
+
+    <h2>Checkbox</h2>
+    <input type="checkbox" name="hobi" value="berenang"> Berenang<br>
+    <input type="checkbox" name="hobi" value="bersepeda"> Bersepeda<br>
+    <input type="checkbox" name="hobi" value="membaca"> Membaca<br>
+    <input type="submit" value="submit" />
+  </body>
+</html>
